ff57fd52ba48c574eb416a8333b6c465576bdf46
Karla G. <k.grajalesluna@gmail.com>
2024-12-26 13:13:37 +0000

    added sum.js && sum.test.js

---  get-ordinal-number.test.js
+++  get-ordinal-number.test.js
@@ -25,65 +25,65 @@ test("convert numbers to ordinal numbers", () => {
 // ============================= function isEven ===============================
 
 
-function isEven(n) {
-  return n % 2 === 0;
-}
-
-test("is Even should return true for an even number", () => {
-  let result = isEven(4);
-  expect(result).toBe(true);
-});
-
-test("is Even should return true for an even number", () => {
-  let result = isEven(6
-
-  );
-  expect(result).toBe(true);
-});
-
-// module.exports = isEven;
-//example on how have to be the files in diferente file 
-
-
-// src/
-//   getOrdinalNumber.js
-//   isEven.js
-// tests/
-//   getOrdinalNumber.test.js
-//   isEven.test.js
-// Archivo: isEven.js
-// javascript
-// Copiar código
 // function isEven(n) {
-//     return n % 2 === 0;
+//   return n % 2 === 0;
 // }
 
-function largestNumbers(num1, num2, num3) {
-  let largest = num1;
-  if (num2 > largest) largest = num2;
-  if (num3 > largest) largest = num3;
-  return largest;
-}
+// test("is Even should return true for an even number", () => {
+//   let result = isEven(4);
+//   expect(result).toBe(true);
+// });
 
-// function largestNumbers(num1, num2, num3) {
-//   return Math.max(num1, num2, num3);
-// }
+// test("is Even should return true for an even number", () => {
+//   let result = isEven(6
 
-console.assert(
-  largestNumbers(5, 3, 7) === 7,
-  "largest number is third position"
-);
-console.assert(
-  largestNumbers(2, 9, 3) === 9,
-  "largest number in second position"
-);
-console.assert(
-  largestNumbers(1.3, 2.7, 3.1) === 3.1,
-  "largest number from decimal numbers"
-);
-console.assert(
-  largestNumbers(-3, -4, -5) === -3,
-  "largest number from decimal numbers"
-);
+//   );
+//   expect(result).toBe(true);
+// });
 
-console.log(largestNumbers(5, 3, 7));
+// // module.exports = isEven;
+// //example on how have to be the files in diferente file 
+
+
+// // src/
+// //   getOrdinalNumber.js
+// //   isEven.js
+// // tests/
+// //   getOrdinalNumber.test.js
+// //   isEven.test.js
+// // Archivo: isEven.js
+// // javascript
+// // Copiar código
+// // function isEven(n) {
+// //     return n % 2 === 0;
+// // }
+
+// // function largestNumbers(num1, num2, num3) {
+// //   let largest = num1;
+// //   if (num2 > largest) largest = num2;
+// //   if (num3 > largest) largest = num3;
+// //   return largest;
+// // }
+
+// // function largestNumbers(num1, num2, num3) {
+// //   return Math.max(num1, num2, num3);
+// // }
+
+// console.assert(
+//   largestNumbers(5, 3, 7) === 7,
+//   "largest number is third position"
+// );
+// console.assert(
+//   largestNumbers(2, 9, 3) === 9,
+//   "largest number in second position"
+// );
+// console.assert(
+//   largestNumbers(1.3, 2.7, 3.1) === 3.1,
+//   "largest number from decimal numbers"
+// );
+// console.assert(
+//   largestNumbers(-3, -4, -5) === -3,
+//   "largest number from decimal numbers"
+// );
+
+// // console.log(largestNumbers(5, 3, 7));
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Ordinal-testing-example
-This repository is to test how Jest work and how use efficiently, adding its dependencies witn npm 
+
+This repository is for testing how Jest works and how to use it efficiently,  adding its dependencies installed via npm.
+
+
+## Instructions
+
+1. **Installation**:
+   ```bash
+   npm install
+
+2. **Running Tests**:
+    ```bash
+    npm test
+
+3. **Using Jest in the Project**
+    * Add more tests by creating new test files under the __tests__ folder. 
+    
+    folder.
+    * Customize Jest configurations in jest.config.js.
+
