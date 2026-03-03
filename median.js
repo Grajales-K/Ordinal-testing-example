7ef8fd8559fd2d42ac86cc84b52e0c4c761451fc
Karla G. <k.grajalesluna@gmail.com>
2026-03-03 21:17:41 +0000

    feat: add logging for median calculation process

--- median.js
+++ median.js
@@ -1,8 +1,14 @@
 function calculateMedian(list) {
   const middleIndex = Math.floor(list.length / 2);
+  console.log(`Middle index: ${middleIndex}`);
+  
   const median = list.splice(middleIndex, 1)[0];
+  console.log(`Median: ${median}`);
+  console.log(list)//splice remove the item, this is not safe because has mutate the array
 
   return median;
 }
 
+
+// console.log(calculateMedian([10, 20, 30, 40, 50]));
 module.exports = calculateMedian;
